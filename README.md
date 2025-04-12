894b148173f6022c0941ef04b07b81a55db71eb7
eugene <81062608+eugene225@users.noreply.github.com>
2025-04-12 15:13:51 +0900

    docs : MongoDB의존성 추가 및 리드미 생성

---
+++ README.md
@@ -0,0 +1,4 @@
+
+#### 🧪 테스트 실험용 저장소
+
+📁 [mongo-index-test](./src/test/kotlin/com/park/kopring/index) - MongoDB CompoundIndex
--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-graphql")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
